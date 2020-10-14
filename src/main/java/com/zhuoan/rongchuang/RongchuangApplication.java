dae7f3ea16c272f349deb89df982d1a01020b15e
luo <993739647@qq.com>
2020-10-14 12:30:38 +0800

    2020-10-14-12

---
+++ src/main/java/com/zhuoan/rongchuang/RongchuangApplication.java
@@ -0,0 +1,13 @@
+package com.zhuoan.rongchuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RongchuangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RongchuangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhuoan/rongchuang/RongchuangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuoan.rongchuang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RongchuangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
